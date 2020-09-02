a84263b7bcd78f939bbb3762c54f352646d63941
Daniel <55975773+DanielBoyko@users.noreply.github.com>
2020-09-03 00:34:48 +0300

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -441,16 +441,4 @@
 
 						});
 
-			postForm = $('#viewers').text();
-			console.log(postForm);
-	$.ajax({ //Process the form using $.ajax()
-		type      : 'POST', //Method type
-		url       : 'https://startupbsnu.000webhostapp.com/viewers.php', //Your form processing file URL
-		data      : postForm, //Forms name
-		dataType  : 'json',
-		success   : function(data) {
-			$('#viewers').text(data);
-		}
-	});
-
 })(jQuery);
